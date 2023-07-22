642021cd37f21526cf22c850bb9876339944ca4a
jihwan98 <jihwan98.dev@gmail.com>
2023-07-23 02:02:10 +0900

    MultipleTable

---
+++ src/chapter_04/_09_MultipleTable.java
@@ -0,0 +1,14 @@
+package chapter_04;
+
+public class _09_MultipleTable {
+    public static void main(String[] args) {
+        // 구구단
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.println(i + " x " + j + " = " + (i * j));
+            }
+            System.out.println();
+        }
+    }
+}
